6bb375aabacb4dae8c935fe7bcd1625ef4f5eeb8
Chris Patuzzo <chris@patuzzo.co.uk>
2018-08-05 17:01:02 +0100

    Add a Planet::zone_height method
    I need to calculate the radius of a planet’s zone
    before constructing the planet because this
    determines where it will be located.

--- src/model/planet/mod.rs
+++ src/model/planet/mod.rs
@@ -4,18 +4,20 @@ use super::circle::Circle;
 #[derive(Clone, Debug, PartialEq)]
 pub struct Planet {
     pub mass: Circle,
+    pub zone: Circle,
     pub ordinal: usize,
 }
 
 impl Planet {
-    pub fn new(location: Point, radius: f64, ordinal: usize) -> Self {
+    pub fn new(location: Point, radius: f64, zone_height: f64, ordinal: usize) -> Self {
         let mass = Circle::new(location, radius);
+        let zone = Circle::new(location, Self::zone_height(radius, zone_height));
 
-        Planet { mass, ordinal }
+        Planet { mass, zone, ordinal }
     }
 
-    pub fn orbital_zone(&self) -> Circle {
-        Circle::new(self.mass.center, self.mass.radius * 3.0)
+    pub fn zone_height(radius: f64, height: f64) -> f64 {
+        radius * (height + 1.0)
     }
 }
 
--- src/model/planet/test.rs
+++ src/model/planet/test.rs
@@ -1,8 +1,10 @@
 use super::*;
 
-fn setup() -> (Point, Planet) {
+type Subject = Planet;
+
+fn setup() -> (Point, Subject) {
     let location = Point::new(0.5, 0.5);
-    let planet = Planet::new(location, 0.1, 3);
+    let planet = Subject::new(location, 0.1, 2.0, 3);
 
     (location, planet)
 }
@@ -20,28 +22,28 @@ mod new {
     }
 
     #[test]
+    fn it_sets_zone_from_location_radius_and_zone_height() {
+        let (location, planet) = setup();
+        let zone = planet.zone;
+
+        assert_eq!(zone.center, location);
+        assert_approx_eq!(zone.radius, 0.3);
+    }
+
+    #[test]
     fn it_sets_ordinal() {
         let (_, planet) = setup();
         assert_eq!(planet.ordinal, 3);
     }
 }
 
-mod orbital_zone {
+mod zone_height {
     use super::*;
 
     #[test]
-    fn it_returns_a_circle_with_the_same_center() {
-        let (location, planet) = setup();
-        let circle = planet.orbital_zone();
-
-        assert_eq!(circle.center, location);
-    }
-
-    #[test]
-    fn it_returns_a_circle_with_three_times_the_radius() {
-        let (_, planet) = setup();
-        let circle = planet.orbital_zone();
-
-        assert_approx_eq!(circle.radius, 0.3);
+    fn it_returns_a_height_relative_to_the_radius_of_the_planet() {
+        assert_approx_eq!(Subject::zone_height(0.1, 2.0), 0.3);
+        assert_approx_eq!(Subject::zone_height(0.1, 3.0), 0.4);
+        assert_approx_eq!(Subject::zone_height(0.2, 3.0), 0.8);
     }
 }
