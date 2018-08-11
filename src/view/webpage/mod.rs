db7bda1f0e5d311258899a642b31483cda9cc720
Chris Patuzzo <chris@patuzzo.co.uk>
2018-08-11 14:21:01 +0100

    Resize canvas to fit window

--- src/view/webpage/mod.rs
+++ src/view/webpage/mod.rs
@@ -1,9 +1,9 @@
 use super::webgl::WebGLRenderingContext;
 
-use stdweb::web::{document, Element};
+use stdweb::web::{document, Element, HtmlElement};
 use stdweb::web::html_element::CanvasElement;
 use stdweb::unstable::TryInto;
-use stdweb::traits::INode;
+use stdweb::traits::{IHtmlElement, INode};
 
 pub struct Webpage {
     pub context: WebGLRenderingContext,
@@ -19,6 +19,7 @@ impl Webpage {
 
         Self::add_to_page(&canvas);
         Self::add_to_page(&style);
+        Self::resize_canvas(&canvas);
 
         Self { context }
     }
@@ -27,6 +28,22 @@ impl Webpage {
         document().create_element("canvas")
             .expect("failed to create element").try_into()
             .expect("failed to convert element to canvas")
+    }
+
+    fn resize_canvas(canvas: &CanvasElement) {
+        let rectangle = Self::body().get_bounding_client_rect();
+        let pixel_ratio = Self::get_device_pixel_ratio();
+
+        let width = rectangle.get_width() as u32 * pixel_ratio;
+        let height = rectangle.get_height() as u32 * pixel_ratio;
+
+        canvas.set_width(width);
+        canvas.set_height(height);
+    }
+
+    fn get_device_pixel_ratio() -> u32 {
+        let ratio = js! { return window.devicePixelRatio; };
+        ratio.try_into().unwrap_or(1)
     }
 
     fn fetch_context(canvas: &CanvasElement) -> WebGLRenderingContext {
@@ -43,6 +60,10 @@ impl Webpage {
     }
 
     fn add_to_page<T: INode>(element: &T) {
-        document().body().expect("failed to fetch body").append_child(element);
+        Self::body().append_child(element);
+    }
+
+    fn body() -> HtmlElement {
+        document().body().expect("failed to fetch body")
     }
 }
