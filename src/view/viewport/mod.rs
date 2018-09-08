6415203d8858ff68b95c9af18382def4f7c4c142
Chris Patuzzo <chris@patuzzo.co.uk>
2018-09-08 12:42:09 +0100

    Extract a Viewport struct with a clear function

---
+++ src/view/viewport/mod.rs
@@ -0,0 +1,22 @@
+use super::webgl::WebGLRenderingContext as GL;
+
+pub struct Viewport {
+    clear_color: (f32, f32, f32, f32),
+}
+
+impl Viewport {
+    pub fn new(red: f32, green: f32, blue: f32, alpha: f32) -> Self {
+        Self { clear_color: (red, green, blue, alpha) }
+    }
+
+    pub fn clear(&self, context: &GL) {
+        let width = context.canvas().width();
+        let height = context.canvas().height();
+
+        let (r, g, b, a) = self.clear_color;
+
+        context.viewport(0, 0, width as i32, height as i32);
+        context.clear_color(r, g, b, a);
+        context.clear(GL::COLOR_BUFFER_BIT);
+    }
+}
